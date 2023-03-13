4ce3a6cce204cb1692ecc8ea397097e863fc4d54
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-13 11:05:17 +0200

    HASHED password (bcrypt) + Added user modal to DB

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import net from "net";
 import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
 
+/* ENVIRONMENT VARIABLES */
+const MONGO_URL = process.env.MONGO_URL;
+
+// Data imports
+import UserModel from "./models/User.js";
+
+/* CONFIGURATION */
 const app = express();
 const appPort = process.env.APP_PORT || 3000;
 
@@ -49,11 +58,6 @@ isPortInUse(appPort)
     console.error(`Error checking if port ${appPort} is in use: ${err}`);
   });
 
-/* CONFIGURATION */
-dotenv.config();
-
-const MONGO_URL = process.env.MONGO_URL;
-
 /* MONGOOSE SETUP */
 mongoose
   .connect(MONGO_URL, {
@@ -74,15 +78,35 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
+/* ROUTES */
 app.get("/", (req, res) => {});
 
-app.post("/auth/register", registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  res.json({
-    success: true,
-  });
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
+  } catch (error) {
+    console.log("🚀 ~ file: index.js:105 ~ app.post ~ error:", error);
+
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 });
