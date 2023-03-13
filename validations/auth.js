0b3d06f43385e17e2c62a1fe984dda258a426663
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-13 11:03:24 +0200

    Update auth.js

--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,10 @@
 import { body } from "express-validator";
 
 export const registerValidation = [
-    body("email", "Email is not in the correct format.").isEmail(),
-    body("password", "Password must be at least 5 characters long.").isLength({ min: 5 }),
-    body("fullName", "Please add your name.").isLength({ min: 3 }),
-    body("avatarUrl", "The URL is not correct.").optional().isURL(),
-    ];
+  body("email", "Email is not in the correct format.").isEmail(),
+  body("password", "Password must be at least 5 characters long.").isLength({
+    min: 5,
+  }),
+  body("fullName", "Please add your name.").isLength({ min: 3 }),
+  body("avatarUrl", "The URL is not correct.").optional().isURL(),
+];
