8cb2ac2e6488aa6c92f090d6e240879f8031ee45
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-14 08:44:54 +0200

    Fix typo

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -2,13 +2,12 @@ import jwt from "jsonwebtoken";
 
 export default function checkAuth(req, res, next) {
   const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
-  console.log("🚀 ~ file: checkAuth.js:5 ~ checkAuth ~ token:", token)
 
   // If a token exists
   if (token) {
     try {
       const decoded = jwt.verify(token, "secret123");
-      req.user = decoded._id;
+      req.userId = decoded._id;
 
       // Move on to next middleware
       next();
