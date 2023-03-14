7e4155b1510247cd4daa52658de463097ecac986
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-14 08:47:43 +0200

    Added loginValidation

--- validations/auth.js
+++ validations/auth.js
@@ -1,5 +1,11 @@
 import { body } from "express-validator";
 
+export const loginValidation = [
+  body("email", "Email is not in the correct format.").isEmail(),
+  body("password", "Password must be at least 5 characters long.").isLength({
+    min: 5,
+  }),
+];
 export const registerValidation = [
   body("email", "Email is not in the correct format.").isEmail(),
   body("password", "Password must be at least 5 characters long.").isLength({
