50a81a6ca02086b1fab3f037c2b95ef6657e860c
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-18 23:05:01 +0200

    Uploaded file functionality has been added

--- Frontend/src/pages/AddPost/index.jsx
+++ Frontend/src/pages/AddPost/index.jsx
@@ -1,7 +1,7 @@
-import { useState, useMemo, useCallback } from "react";
+import { useState, useMemo, useCallback, useRef } from "react";
 import { useSelector } from "react-redux";
 import { selectIsAuth } from "../../redux/slices/auth";
-
+import axios from "../../api/axios";
 import { Button, Paper, TextField } from "@mui/material";
 
 import SimpleMDE from "react-simplemde-editor";
@@ -11,13 +11,39 @@ import styles from "./AddPost.module.scss";
 import { Navigate } from "react-router-dom";
 
 export const AddPost = () => {
+  const inputFileRef = useRef(null);
   const isAuth = useSelector(selectIsAuth);
-  const imageUrl = "";
+
+  const [title, setTitle] = useState("");
   const [value, setValue] = useState("");
+  const [tags, setTags] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = async (event) => {
+    try {
+      const file = event.target.files[0];
 
-  const onClickRemoveImage = () => {};
+      if (!file) {
+        console.warn("No file selected");
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("image", file, file.name); // Add the filename property here
+      debugger;
+      const { data } = await axios.post("/uploads", formData);
+      debugger;
+      console.log(data);
+      setImageUrl(data.url);
+    } catch (error) {
+      console.warn(error);
+      alert("Ошибка при загрузке файла");
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl("");
+  };
 
   const onChange = useCallback((value) => {
     setValue(value);
@@ -44,21 +70,35 @@ export const AddPost = () => {
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-      )}
-      {imageUrl && (
-        <img
-          className={styles.image}
-          src={`http://localhost:4444${imageUrl}`}
-          alt="Uploaded"
-        />
+        <>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Удалить
+          </Button>
+
+          <img
+            className={styles.image}
+            src={`http://localhost:4444${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
       )}
       <br />
       <br />
@@ -66,9 +106,13 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Заголовок статьи..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         fullWidth
       />
       <TextField
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
         classes={{ root: styles.tags }}
         variant="standard"
         placeholder="Тэги"
