e1bc5647a1eb4dc76f933e845ba6de110f20a83e
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-19 14:33:50 +0200

    Update index.jsx

--- Frontend/src/components/UserInfo/index.jsx
+++ Frontend/src/components/UserInfo/index.jsx
@@ -1,9 +1,13 @@
-import styles from './UserInfo.module.scss';
+import styles from "./UserInfo.module.scss";
 
 export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
   return (
     <div className={styles.root}>
-      <img className={styles.avatar} src={avatarUrl || '/noavatar.png'} alt={fullName} />
+      <img
+        className={styles.avatar}
+        src={avatarUrl || "/noavatar.png"}
+        alt={fullName}
+      />
       <div className={styles.userDetails}>
         <span className={styles.userName}>{fullName}</span>
         <span className={styles.additional}>{additionalText}</span>
@@ -11,3 +15,4 @@ export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
     </div>
   );
 };
+
