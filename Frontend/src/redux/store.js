7ef471649f44483035e23d2195c293e1c82308cb
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-19 14:09:07 +0200

    Code explanations

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,12 +1,15 @@
+// Import configureStore from Redux Toolkit
 import { configureStore } from "@reduxjs/toolkit";
+// Import the posts reducer from the posts slice
 import { postsReducer } from "./slices/posts";
+// Import the auth reducer from the auth slice
 import { authReducer } from "./slices/auth";
 
-const store = configureStore({
+// Create and export a Redux store using the configureStore function
+// Combine the reducers for posts and auth into the rootReducer
+export default configureStore({
   reducer: {
-    posts: postsReducer,
-    auth: authReducer,
+    posts: postsReducer, // Assign the posts reducer to the posts state
+    auth: authReducer,   // Assign the auth reducer to the auth state
   },
 });
-
-export default store;
