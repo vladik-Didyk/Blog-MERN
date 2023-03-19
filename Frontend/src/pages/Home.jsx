e5a73cb1e2fd0e27a966aadfd4e0727672502048
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-19 14:13:30 +0200

    Optimized and minimized the code

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts, fetchTags } from "../redux/slices/posts";
-
 import { Grid, Tabs, Tab } from "@mui/material/";
-
 import { Post } from "../components/Post";
 import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
@@ -12,52 +10,47 @@ export const Home = () => {
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.auth.data);
   const { posts, tags } = useSelector((state) => state.posts);
-
   const isPostsLoading = posts.status === "loading";
   const isTagsLoading = tags.status === "loading";
 
   useEffect(() => {
     dispatch(fetchPosts());
     dispatch(fetchTags());
-  }, []);
+  }, [dispatch]);
+
+  const renderPost = (element, index) => {
+    const isLoading = isPostsLoading;
+    const postProps = isLoading
+      ? { isLoading }
+      : {
+          id: element._id,
+          title: element.title,
+          imageUrl: element.imageUrl
+            ? `${process.env.REACT_APP_BACKEND_PORT}${element.imageUrl}`
+            : "",
+          user: element.user,
+          createdAt: element.createdAt,
+          viewsCount: element.viewCount,
+          commentsCount: 3,
+          isLoading,
+          tags: element.tags,
+          isEditable: userData?._id === element.user._id,
+        };
+
+    return <Post key={index} {...postProps} />;
+  };
+
   return (
     <>
-      <Tabs
-        style={{ marginBottom: 15 }}
-        value={0}
-        aria-label="basic tabs example"
-      >
+      <Tabs value={0} aria-label="basic tabs example" style={{ marginBottom: 15 }}>
         <Tab label="Новые" />
         <Tab label="Популярные" />
       </Tabs>
       <Grid container spacing={4}>
-        <Grid xs={8} item>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map(
-            (element, index) =>
-              isPostsLoading ? (
-                <Post key={index} isLoading={true} />
-              ) : (
-                <Post
-                  key={index}
-                  id={element._id}
-                  title={element.title}
-                  imageUrl={
-                    element.imageUrl
-                      ? `${process.env.REACT_APP_BACKEND_PORT}${element.imageUrl}`
-                      : ""
-                  }
-                  user={element.user}
-                  createdAt={element.createdAt}
-                  viewsCount={element.viewCount}
-                  commentsCount={3}
-                  isLoading={isPostsLoading}
-                  tags={element.tags}
-                  isEditable={userData?._id === element.user._id}
-                />
-              )
-          )}
+        <Grid item xs={8}>
+          {(isPostsLoading ? [...Array(5)] : posts.items).map(renderPost)}
         </Grid>
-        <Grid xs={4} item>
+        <Grid item xs={4}>
           <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
@@ -83,4 +76,3 @@ export const Home = () => {
     </>
   );
 };
-
