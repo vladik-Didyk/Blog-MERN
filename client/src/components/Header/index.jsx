e5cd95bbe4a7cb8aa2ed6d6ad5d66f7ea8a49344
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-21 14:18:24 +0200

    Update index.jsx

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>ARCHAKOV BLOG</div>
+            <div>Vladik Didyk</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
